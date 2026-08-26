fb7dc031572e68fa6e005fed78b050bbd7a68aed
배정한 <thank0906411@gmail.com>
2026-08-26 16:20:40 +0900

    versionCode 3으로 상향: 언어 선택 기능 릴리즈 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.senkiro.translateapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
     }
 
     // CI(GitHub Actions)에서 환경변수로 keystore 정보를 주입한다.
